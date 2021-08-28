dfb6def59e39cb8d59e1b8b175118f259fc4076d
Ewa Zgorzelska <ewa.naroznik@hotmail.com>
2021-08-28 12:25:43 +0200

    adding modal

--- src/views/StartView/StartView.js
+++ src/views/StartView/StartView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     StartViewContainer,
     GameTitle,
@@ -9,8 +9,17 @@ import {
 } 
     from './StartViewStyles';
 import { Button } from 'components/atoms/Button/Button';
+import MainModal from 'components/organisms/MainModal/MainModal';
 
 const StartView = () => {
+
+    const [isOpen, setOpen] = useState(false);
+
+    // const showModal = () => setOpen(true);
+    // const closeModal = () => setOpen(!isOpen);
+
+    const toggleModal = () => setOpen(!isOpen);
+
     return (
         <StartViewContainer>
             <GameTitle>Ducks vs Foxes</GameTitle>
@@ -19,9 +28,10 @@ const StartView = () => {
                 <Animal2 />
             </AnimalWrapper>            
             <ButtonsWrapper>
-                <Button>Quack</Button>
+                <Button onClick={toggleModal}>Quack</Button>
                 <Button>Woof</Button>
             </ButtonsWrapper>
+            <MainModal isOpen={isOpen} toggleModal={toggleModal}/>
         </StartViewContainer>
     )
 }
