4115e3b666451900e9f56aca65433a761b5a8af9
Ewa Zgorzelska <ewa.naroznik@hotmail.com>
2021-08-29 16:28:57 +0200

    splitting images function

--- src/views/GameView/GameView.js
+++ src/views/GameView/GameView.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useContext }from 'react';
 //import styled from 'styled-components';
+//import { AppContext } from 'AppContext/AppContext';
+import { cutImageUp } from './getPuzzleElement';
 
 const GameView = () => {
-
+    //const { animalData } = useContext(AppContext);
+    const pieces = cutImageUp();
+    const source = pieces[0];
+    
     return (
-        <div>
-            Hello World!
-        </div>
+        <>
+            <img id="piece" src={source} alt="cos"/>
+            <img id="piece" src={source} alt="cos1"/>
+            <img id="piece" src={source} alt="cos2"/>
+            <img id="piece" src={source} alt="cos3"/>
+        </>
     )
 }
 
